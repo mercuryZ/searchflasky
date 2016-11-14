b593ccca8801f61b6350b68021851809b55b4c22
mercuryZ <15210820708@163.com>
2016-11-15 00:14:56 +0800

    bootstrap

--- hello.py
+++ hello.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 
 # @app.route("/<name>")
