ca5e7ff594bcb15667e6d3271795496a3eec5fa5
zangree <zangree@163.com>
2016-11-14 13:22:59 +0800

    add bootstrap

--- hello.py
+++ hello.py
@@ -1,24 +1,20 @@
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 manager = Manager(app)
-
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
 
-# @app.route("/<name>")
-# def index1(name):
-#     return '<h1>Hello, %s!</h1>' % name
-
-
-@app.route("/<int:num>")
-def index2(num):
-    return '<h1>Hello, %s!</h1>' % str(num)
+@app.route("/<name>")
+def index1(name):
+    return '<h1>Hello, %s!</h1>' % name
 
 
 if __name__ == "__main__":
