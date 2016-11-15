047908d65068e4aa875fa9078084f3757b6c54e1
zangree <zangree@163.com>
2016-11-15 17:34:00 +0800

    404 500

--- hello.py
+++ hello.py
@@ -10,12 +10,21 @@ bootstrap = Bootstrap(app)
 
 @app.route("/")
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 
 @app.route("/<name>")
 def index1(name):
     return '<h1>Hello, %s!</h1>' % name
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
