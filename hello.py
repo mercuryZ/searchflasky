8fe24ff0f0bfcaff989fe5acfcf92a6064c18f95
zangree <zangree@163.com>
2016-11-16 20:04:42 +0800

    sqlalchemy 0

--- hello.py
+++ hello.py
@@ -1,15 +1,25 @@
+import os
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SECRET_KEY'] = 'balabalaxiaomoxian'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
-app.config['SECRET_KEY'] = 'balabalaxiaomoxian'
+db = SQLAlchemy(app)
+
 
 
 class NameForm(FlaskForm):
