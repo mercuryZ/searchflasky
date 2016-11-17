f628c94ed79c07a897dfdfd1b3fef6408120fbed
zangree <zangree@163.com>
2016-11-17 18:06:22 +0800

    before sqlalchemy

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
@@ -22,9 +22,29 @@ db = SQLAlchemy(app)
 
 
 
-class NameForm(FlaskForm):
+class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField("Submit")
+
+
+class Role(db.Model):
+    __table__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -32,9 +52,17 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ""
-    return render_template('index.html', form=form, name=name)
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),known=session.get('known', False))
 
 
 @app.route("/<name>")
